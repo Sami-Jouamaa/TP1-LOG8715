3ca0602b90eca650bbdce43efe8c51cc5162862c
Sami-Jouamaa <samijouamaa@gmail.com>
2026-01-20 14:41:42 -0500

    collisions fixed

--- TP1-H26/Assets/Systems/CollisionManagement.cs
+++ TP1-H26/Assets/Systems/CollisionManagement.cs
@@ -1,19 +1,89 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionManagement : ISystem
 {
-    public string Name => "CollisionManager";
+    public ECSController controller = ECSController.Instance;
+
+    public string Name => "CollisionManagement";
 
     public void UpdateSystem()
     {
         Dictionary<uint, Vector2> positionDictionary = Positions.circlePositions;
-        // (x2 - x1)^2 + (y2 - y1) <= (r1 + r2)^2
+        Dictionary<uint, int> sizesDictionary = Sizes.sizes;
+        Dictionary<uint, Vector2> velocities = Velocities.velocities;
+
         for (uint i = 0; i < Positions.circlePositions.Count; i++)
         {
             for (uint j = i + 1; j < Positions.circlePositions.Count; j++)
             {
-                return;
+                float xDifference = Mathf.Pow(positionDictionary[i].x - positionDictionary[j].x, 2);
+                float yDifference = Mathf.Pow(positionDictionary[i].y - positionDictionary[j].y, 2);
+                float radiiSum = Mathf.Pow(sizesDictionary[i] + sizesDictionary[j], 2);
+
+                if (xDifference + yDifference <= radiiSum)
+                {
+                    if (CollisionBehavior.behaviors[i] == Behavior.Dynamic && CollisionBehavior.behaviors[j] == Behavior.Dynamic)
+                    {
+                        if (sizesDictionary[i] <= controller.Config.protectionSize && sizesDictionary[i] == sizesDictionary[j])
+                        {
+                            CollisionCount.collisionCount[i] += 1;
+                            CollisionCount.collisionCount[j] += 1;
+                        }
+
+                        int minSize = (int)MathF.Min(sizesDictionary[i], sizesDictionary[j]);
+                        if (sizesDictionary[i] != sizesDictionary[j])
+                        {
+                            if (minSize == sizesDictionary[i])
+                            {
+                                Sizes.sizes[i] -= 1;
+                                Sizes.sizes[j] += 1;
+                            }
+                            else
+                            {
+                                Sizes.sizes[i] += 1;
+                                Sizes.sizes[j] -= 1;
+                            }
+                        }
+                    }
+
+                    if (sizesDictionary[i] == 0)
+                    {
+                        controller.DestroyShape(i);
+                    }
+                    if (sizesDictionary[j] == 0)
+                    {
+                        controller.DestroyShape(j);
+                    }
+
+                    CollisionResult resultingPosVel = CollisionUtility.CalculateCollision(
+                        positionDictionary[i],
+                        velocities[i],
+                        sizesDictionary[i],
+                        positionDictionary[j],
+                        velocities[j],
+                        sizesDictionary[j]
+                    );
+
+                    Vector2 newPos1 = resultingPosVel.position1;
+                    Vector2 newVel1 = resultingPosVel.velocity1;
+
+                    Vector2 newPos2 = resultingPosVel.position2;
+                    Vector2 newVel2 = resultingPosVel.velocity2;
+
+                    if (CollisionBehavior.behaviors[i] == Behavior.Dynamic)
+                    {
+                        controller.UpdateShapePosition(i, newPos1);
+                        Velocities.velocities[i] = newVel1;
+                    }
+                    if (CollisionBehavior.behaviors[j] == Behavior.Dynamic)
+                    {
+                        controller.UpdateShapePosition(j, newPos2);
+                        Velocities.velocities[j] = newVel2;
+                    }
+
+                }
             }
         }
     }
