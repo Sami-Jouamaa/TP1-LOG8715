3a4851a2ea712fb436c1f581e8f97300e59996d6
Jeremy Trudel <jeremytrudel78@gmail.com>
2026-01-20 14:29:08 -0500

    fix

--- TP1-H26/Assets/Systems/BehaviorManagement.cs
+++ TP1-H26/Assets/Systems/BehaviorManagement.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
-public class BehaviorManagement
+public class BehaviorManagement : ISystem
 {
     public ECSController controller = ECSController.Instance;
-    
+
+    public string Name => throw new System.NotImplementedException();
+
     public void UpdateSystem()
     {
         for (uint i = 0; i < Sizes.sizes.Count; i++)
         {
+
             if (CollisionCount.collisionCount[i] == controller.Config.protectionCollisionCount)
             {
-                
+                CollisionBehavior.behaviors[i] = Behavior.Protected;
             }
+
         }
-        
     }
 }
