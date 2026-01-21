29e7d50264f3352a73b3296ca01ef4d7e345543c
Sami-Jouamaa <samijouamaa@gmail.com>
2026-01-21 14:10:17 -0500

    circles now bounce off the screen borders

--- TP1-H26/Assets/Systems/CollisionManagement.cs
+++ TP1-H26/Assets/Systems/CollisionManagement.cs
@@ -16,11 +16,34 @@ public class CollisionManagement : ISystem
 
         for (uint i = 0; i < Positions.circlePositions.Count; i++)
         {
+            // Collision with screen border
+            float currentX = positionDictionary[i].x;
+            float currentY = positionDictionary[i].y;
+            float radius = sizesDictionary[i] / 2;
+
+            float maxY = 15;
+            float minY = -15;
+            float maxX = 31;
+            float minX = -31;
+
+            Vector2 currentVel = Velocities.velocities[i];
+            if (currentX + radius >= maxX || currentX - radius <= minX)
+            {
+                currentVel.x *= -1;
+                Velocities.velocities[i] = currentVel;
+            }
+            if (currentY + radius >= maxY || currentY - radius <= minY)
+            {
+                currentVel.y *= -1;
+                Velocities.velocities[i] = currentVel;
+            }
+
             for (uint j = i + 1; j < Positions.circlePositions.Count; j++)
             {
+                // Collision with other circle
                 float xDifference = Mathf.Pow(positionDictionary[i].x - positionDictionary[j].x, 2);
                 float yDifference = Mathf.Pow(positionDictionary[i].y - positionDictionary[j].y, 2);
-                float radiiSum = Mathf.Pow(sizesDictionary[i] + sizesDictionary[j], 2);
+                float radiiSum = Mathf.Pow(sizesDictionary[i] / 2 + sizesDictionary[j] / 2, 2);
 
                 if (xDifference + yDifference <= radiiSum)
                 {
