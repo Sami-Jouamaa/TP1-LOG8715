0f2891a3683bdec0bb682a312c267b3ea8f137d4
Jérémy <jeremytrudel78@gmail.com>
2026-01-24 19:39:48 -0500

    collisionPair distinction

--- TP1-H26/Assets/Components/CollisionPair.cs
+++ TP1-H26/Assets/Components/CollisionPair.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-public class CollisionPairs : IComponent
+public class CollisionPair : IComponent
 {
     public static Dictionary<uint, uint> CollisionPairs = new Dictionary<uint, uint>();
 }
